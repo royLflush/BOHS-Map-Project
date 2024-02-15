a5d963614baf7d7cb2b7a24e22d6432af442756c
royLflush <97312135+royLflush@users.noreply.github.com>
2024-02-15 09:04:52 -0800

    Update game.js

--- game.js
+++ game.js
@@ -5,4 +5,5 @@ function setup() {
 function draw() {
   background(220);
   //This is where we will paste all the code stuff we do from p5 so that everyone has the same copy
+  //Click on the pencil on the right corner to edit the code.
 }
